96530c63a20c4130b0e3b80aba79f6621c620986
irakliboch <irakli.bochorishvili.2@gmail.com>
2020-12-08 12:30:37 +0100

    Create life_in_weeks_calc.py

---
+++ life_in_weeks_calc.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+years_left = 90 - int(age)
+weeks = years_left * 52
+days = years_left * 365
+months = years_left * 12
+
+
+
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left. ")
+
+
+
